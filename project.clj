b59a24e8005f6e8fca588994b77812a9c9f7715c
Jukka Villstedt <jvillste@hotmail.com>
2013-05-26 19:12:22 +0300

    x

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject gift-list "0.0.1"
-  :dependencies [[org.clojure/clojure "1.5.1"]
+  :dependencies [[org.clojure/clojure "1.4.0"]
                  [ring/ring-jetty-adapter "1.1.0"]
                  [compojure "1.1.5"]
                  [shoreleave/shoreleave-remote-ring "0.3.0"]
