9c3a89aa0d83de2c94fed47ab751e8000d9010dd
Jukka Villstedt <jvillste@hotmail.com>
2013-05-28 07:33:38 +0300

    x

--- project.clj
+++ project.clj
@@ -11,9 +11,11 @@
 
   :min-lein-version "2.0.0"
   :hooks [leiningen.cljsbuild]
-  :plugins [[lein-cljsbuild "0.3.2"]]
+  :plugins [[lein-cljsbuild "0.2.7" #_"0.3.2"]]
+  :main gift-list.web
   :cljsbuild  {:builds
-               [{:source-paths ["src"]
+               [{;; :source-paths ["src"]
+                 :source-path "src"
                  :compiler {:pretty-print true
                             :output-to "resources/public/js/cljs.js"
                             :optimizations :whitespace}
