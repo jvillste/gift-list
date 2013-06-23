fd14b4dba9cfd0d2bfb23d2f842d7d71f913ac90
Jukka Villstedt <jvillste@hotmail.com>
2013-06-23 17:50:10 +0300

    x

--- src/gift_list/initialize.clj
+++ src/gift_list/initialize.clj
@@ -2,12 +2,9 @@
   (:require [gift-list.data :as data]))
 
 (defn -main
-  ([uri file-name]
-     (data/connect-to-db uri)
+  ([file-name]
+     (data/connect-to-db)
      (data/reset (eval (read-string (slurp file-name)))))
-  ([uri]
-     (data/connect-to-db uri)
-     (data/reset data/defaults))
   ([]
      (data/connect-to-db)
      (data/reset data/defaults)))
